facda7a8e4a900f7655c272a4134e8d7b842d442
Lirurin <zerosouler@gmail.com>
2019-06-05 12:03:02 +0300

    code cleanup

--- js/main.js
+++ js/main.js
@@ -1,99 +1,88 @@
 document.addEventListener("DOMContentLoaded", function(){
-$(window).bind("load", function() {
-    $('.form').submit(function(e) { 
-    
-        e.preventDefault(); 
-        var form_data = $(this).serialize(); 
-        
-        $.ajax({ 
-        type: "POST", 
-        url: "../bookform.php", 
-        data: form_data, 
-        success: function() { 
-        setTimeout( function() { 
-            alert("Ваше сообщение успешно отправлено!"); 
-        }, 1000); }, 
-        error: function() { 
-        setTimeout( function() { 
-            alert("Ошибка отправления!"); 
-        }, 1000); 
-    } 
-        }); 
+    // booking form request
+    $(window).bind("load", function() {
+        $('.form').submit(function(e) { 
+            e.preventDefault(); 
+            var form_data = $(this).serialize();  
+            $.ajax({ 
+                type: "POST", 
+                url: "../bookform.php", 
+                data: form_data, 
+                success: function() { 
+                    setTimeout( function() { 
+                        alert("Ваше сообщение успешно отправлено!"); 
+                    }, 1000); }, 
+                error: function() { 
+                    setTimeout( function() { 
+                        alert("Ошибка отправления!"); 
+                    }, 1000); 
+                } 
+            }); 
         });
-        
-    $(document).ready(function(){
-        $(".container__burger").on("click", function(){
-            $(".content__nav").toggleClass("active");
-            $(".container__burger").toggleClass("active");
+
+        $(document).ready(function(){
+            $(".container__burger").on("click", function(){
+                $(".content__nav").toggleClass("active");
+                $(".container__burger").toggleClass("active");
+            });
         });
     });
 
-});
-// free hosting  
-window.onload = function() { 
+    // free hosting elements deletion 
     try { 
         document.getElementsByClassName("cbalink")[0].parentNode.removeChild(document.getElementsByClassName("cbalink")[0]); 
         document.getElementsByClassName("cumf_bt_form_wrapper")[0].parentNode.removeChild(document.getElementsByClassName("cumf_bt_form_wrapper")[0]); 
     } catch(e) { 
         //catch 
     } 
-}
 
-// anchor scroll
-window.onload = function () {
-    anchors = [].slice.call(document.querySelectorAll('a[href^="#"]'));
-    anchors.forEach(function (anchor) {
-      anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        var anchorOffset = document.querySelector(this.getAttribute('href')).offsetTop;
-        window.scrollTo(0, anchorOffset - 7 * window.innerHeight / 100);
-      });
-    });
-  };
+    // anchor scroll
+    window.onload = function () {
+        anchors = [].slice.call(document.querySelectorAll('a[href^="#"]'));
+        anchors.forEach(function (anchor) {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            var anchorOffset = document.querySelector(this.getAttribute('href')).offsetTop;
+            window.scrollTo(0, anchorOffset - 7 * window.innerHeight / 100);
+        });
+        });
+    };
 
-var botbar = document.getElementsByClassName("top");
-var topCont = document.getElementsByClassName("top__container");
-var navItem = document.getElementsByClassName("navbar__item");
-var contBur = document.getElementsByClassName("container__burger");
-var prevOffset = window.pageYOffset;
+    // variables
+    var botbar = document.getElementsByClassName("top");
+    var topCont = document.getElementsByClassName("top__container");
+    var navItem = document.getElementsByClassName("navbar__item");
+    var contBur = document.getElementsByClassName("container__burger");
+    var prevOffset = window.pageYOffset;
 
-
-//menu resize
-window.onscroll = function myFnc() {
-    if (prevOffset == 0) {
-        var newOffset = window.pageYOffset;
-        if (newOffset > prevOffset) {
-            botbar[0].classList.add("top-move");
-            topCont[0].classList.add("top__container-move");
-            navItem[0].classList.add("navbar__item-move");
-            contBur[0].classList.add("container__burger-move");
+    //menu resize
+    window.onscroll = function myFnc() {
+        if (prevOffset == 0) {
+            var newOffset = window.pageYOffset;
+            if (newOffset > prevOffset) {
+                botbar[0].classList.add("top-move");
+                topCont[0].classList.add("top__container-move");
+                navItem[0].classList.add("navbar__item-move");
+                contBur[0].classList.add("container__burger-move");
+            } else {
+                botbar[0].classList.remove("top-move");
+                topCont[0].classList.remove("top__container-move");
+                navItem[0].classList.remove("navbar__item-move");
+                contBur[0].classList.remove("container__burger-move");
+            }  
         } else {
-            botbar[0].classList.remove("top-move");
-            topCont[0].classList.remove("top__container-move");
-            navItem[0].classList.remove("navbar__item-move");
-            contBur[0].classList.remove("container__burger-move");
-        }  
-    } else {
-        var newOffset = window.pageYOffset;
-        if (newOffset == 0) {
-            botbar[0].classList.remove("top-move");
-            topCont[0].classList.remove("top__container-move");
-            navItem[0].classList.remove("navbar__item-move");
-            contBur[0].classList.remove("container__burger-move");
-        } else {
-            botbar[0].classList.add("top-move");
-            topCont[0].classList.add("top__container-move");
-            navItem[0].classList.add("navbar__item-move");
-            contBur[0].classList.add("container__burger-move");
+            var newOffset = window.pageYOffset;
+            if (newOffset == 0) {
+                botbar[0].classList.remove("top-move");
+                topCont[0].classList.remove("top__container-move");
+                navItem[0].classList.remove("navbar__item-move");
+                contBur[0].classList.remove("container__burger-move");
+            } else {
+                botbar[0].classList.add("top-move");
+                topCont[0].classList.add("top__container-move");
+                navItem[0].classList.add("navbar__item-move");
+                contBur[0].classList.add("container__burger-move");
+            }
         }
-    }
-};
-
-// if(navigator.userAgent.match(/Trident\/7\./)) { // if IE
-//     alert('fuck')
-//     backgrounds = document.querySelector('fixed');
-//     background.style["background-attachment"] = "scroll";
-
-// }
-
+    };
 });
